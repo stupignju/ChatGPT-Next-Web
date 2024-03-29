609dc424b98a81db4a0880eec8d8801bd8ca8d70
stupigcalvin <stupigcalvin@gmail.com>
2024-03-29 15:40:10 +0800

    fix

--- run.sh
+++ run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+source ~/.bashrc
 
 ollama pull qwen:7b
 ollama pull gemma:2b
 nohup ollama serve >ollama.serve.log &
+
+nvm install node
+npm install --global yarn
 yarn install
 nohup yarn dev >front.serve.log &
